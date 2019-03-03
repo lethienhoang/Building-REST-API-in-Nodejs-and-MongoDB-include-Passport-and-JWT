550f26a804a97ca45b3d24adc01e15fd528b7284
lethienhoang <45428011+lethienhoang@users.noreply.github.com>
2019-03-03 15:35:14 +0800

    Fixed - can't build webpack

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ const config = require('../configs/enviroment.config');
 const env = config.envConfig(process.env.NODE_ENV);
 const app = express();
 
-app.listen(env.PORT, err => {
+app.listen(env.PORT, err => {    
     if(err) {
+        console.log(err);
         throw err;
     } else {
-        console.log(`Server running on port: ${PORT} --- running on ${process.env.NODE_ENV}`)
+        console.log(`Server running on port: ${env.PORT} --- running on ${process.env.NODE_ENV}`)
     }
 });
 
